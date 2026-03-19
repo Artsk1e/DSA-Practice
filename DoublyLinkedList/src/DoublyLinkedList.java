b0c563f2c9c86c82c20c9e050db0a5e9b8b353f5
Art Vincent D. Papolonias <artvincent.papolonias@g.msuiit.edu.ph>
2026-03-19 16:20:30 +0800

    Doubly linkedList and reversing.

--- DoublyLinkedList/src/DoublyLinkedList.java
+++ DoublyLinkedList/src/DoublyLinkedList.java
@@ -1,4 +1,4 @@
-import javax.print.attribute.standard.MediaSize;
+
 
 class Node{
     int data;
@@ -11,46 +11,51 @@ class Node{
         this.prev = null;
     }
 }
-public class DoublyLinkedList {
+
+public class DoublyLinkedList{
     Node head;
 
     public void insertAtFront(int newData){
         Node newNode = new Node(newData);
-        if (head != null) {
+
+        if (head != null){
             newNode.next = head;
             head.prev = newNode;
         }
         head = newNode;
+        System.out.println("Added " +  newData  + " to the front");
     }
 
     public void insertAtEnd(int newData){
-
         Node newNode = new Node(newData);
-        if(head == null){
+        if (head == null){
             head = newNode;
+            System.out.println("Added " + newData + " to the list");
             return;
         }
         Node current = head;
-        while(current.next != null){
+        while (current.next != null){
             current = current.next;
         }
-
         current.next = newNode;
         newNode.prev = current;
+        System.out.println("Added " + newData + " to the end");
     }
 
     public void displayInfo(){
         Node current = head;
+        System.out.println("Current LinkedList: ");
         while (current != null){
-            System.out.print("["+ current.data+ "] <-> ");
+            System.out.print("[" + current.data + "] <-> ");
             current = current.next;
         }
         System.out.print("null");
     }
 
     public void reverseDisplay(){
-        if(head == null) return;
         Node current = head;
+        System.out.println("Reverse LinkedList: ");
+        if (head == null) {return;}
 
         while (current.next != null){
             current = current.next;
@@ -60,6 +65,6 @@ public class DoublyLinkedList {
             System.out.print("[" + current.data + "] <-> ");
             current = current.prev;
         }
-        System.out.println("null");
+        System.out.print("null");
     }
 }
---
+++ SinglyLinkedList/src/README.md.txt
@@ -0,0 +1,14 @@
+Reversing a LinkedList.
+
+LeetCode Solution: https://leetcode.com/problems/reverse-linked-list/solutions/7664239/reversing-a-linkedlist-by-artskie123-7ban/
+Problem 206.
+Easy Difficulty
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+data structures implemented: 
+Doubly Linked List: Insert(Front/End), reverse, display.
+
+## 📝 Reflection on LeetCode #206
+Solving this taught me the **"Three-Pointer Dance"**. 
+The biggest challenge was understanding the `nextTemp` bookmark—if you don't save the next node before flipping the pointer, you break the bridge to the rest of the list!
