3483aa77dc09fb8a8599de9efd258a851638ef57
Art Vincent D. Papolonias <artvincent.papolonias@g.msuiit.edu.ph>
2026-04-07 20:35:31 +0800

    Comprehensive Recursion Study: sum, factorial, power, and fibonacci with Big O analysis

--- RecursionPractice/src/Main.java
+++ RecursionPractice/src/Main.java
@@ -5,9 +5,17 @@ public class Main {
         int result = sum(number);
         System.out.println("The recursive sum of " + number + " is: " + result);
 
-        System.out.println(factorial(7));
-        System.out.println(power(2, 8));
+        System.out.println("Factorial of 7: " + factorial(7));
+        System.out.println("Power of 8 with a base of 2: "+power(2, 8));
+        System.out.println("Fibonacci Sequence of 5: "+ fibonacci(5));
 	}
+
+    private static int fibonacci(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 
     private static int factorial(int num) {
         if( num < 1) return 1; //base Case
@@ -26,7 +34,7 @@ public class Main {
         }
         //Recursive Call
         return n + sum(n-1);
-    }
+}
 
 
 }
